2e129c78c63fdc718d77d3ab24ef8620dbba5871
kane.butler3 <rendaroadkill@gmail.com>
2026-06-29 13:01:54 +1000

    hopefully basic obstacle avoidance

--- Code/main.py
+++ Code/main.py
@@ -42,4 +42,17 @@ while True:
                 bot.motor_left(0)
                 bot.motor_right(0)
                 break
+distance = 0
 
+def on_forever():
+    global distance
+    distance = Maqueen.read_ultrasonic(DigitalPin.P13, DigitalPin.P14)
+    basic.show_number(distance)
+    if distance < 15 and distance != 0:
+        Maqueen.control_motor_stop(Maqueen.MyEnumMotor.ALL_MOTOR)
+        basic.pause(1000)
+    else:
+        Maqueen.control_motor(Maqueen.MyEnumMotor.ALL_MOTOR,
+            Maqueen.MyEnumDir.FORWARD,
+            20)
+basic.forever(on_forever)
