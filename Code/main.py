a5bc265a2d145d271792f828f4278501cf026704
jredb <liveofdead5@hotmail.com>
2026-08-06 09:42:01 +1000

    Added comments to line following code, deleted all of the object avoidance code cus it sucks, attempting to set up a systsem where it only avoids objects after seeing them 10 times. Maybe this will help with false positives. Maybe I'm going to do unspeakable things to this robot.

--- Code/main.py
+++ Code/main.py
@@ -7,23 +7,31 @@ import utime as u
 bot = Maqueen()
 fast = 255
 slow = 50
-slowest = 200
+slowest = 200 # This isn't actually the slowest speed, but I can't be bothered to refactor in thonny.
+seecount = 0
 lastdir = True # True is Right, False is Left
 # Turn commands
 while True:
-    if microphone.current_event() == SoundEvent.LOUD or button_a.was_pressed():
+    if microphone.current_event() == SoundEvent.LOUD or button_a.was_pressed(): # Turn on main loop if microphone detects sound or A is pressed
         while True:
             distance_in_cm = bot.ultrasound_measure()
             print(distance_in_cm)
-            if distance_in_cm in (-1, -2):
-                print('NULL DISTANCE!!!')
-            if distance_in_cm in (0, 4):
-                print('OBSTACLE!!!!')
-                objectavoidance.avoid()
+            if distance_in_cm in (0, 0.5):
+                while seecount < 10: # If we've seen it less than 10 times, measure again.
+                    distance_in_cm = bot.ultrasound_measure()
+                    if distance_in_cm in (0, 3):
+                        seecount += 1
+                    if distance_in_cm > 3:
+                        break
+                        seecount = 0 # If its more than 3 cm away, ignore and set sightings to 0.
+                if seecount >= 10:
+                    with open("output.txt", "w", encoding="utf-8") as file:
+                    file.write(f"Avoided object at {distance_in_cm} after {seecount} sightings")
+                    objectavoidance.avoid()
+                    seecount = 0
                 
-            # Full white
+            # Full white detected aka no line
             if bot.line_left() and bot.line_right():
-                poopy = 1
                 bot.left(0)
                 bot.right(0)
                 if not lastdir:
@@ -33,21 +41,21 @@ while True:
                     bot.left(slowest, 1)
                     bot.right(0)
                 
-            # White left
+            # White left, line right
             if not bot.line_left() and bot.line_right():
                 bot.right(fast)
                 bot.left(slow, 1)
                 lastdir = 1
-            # White right
+            # White right, line left
             if not bot.line_right() and bot.line_left():
                 bot.left(fast)
                 bot.right(slow, 1)
                 lastdir = 0
-            # Full black
+            # Full line
             if not bot.line_left() and not bot.line_right():
                 bot.right(fast)
                 bot.left(fast)
-            if button_a.was_pressed():
+            if button_a.was_pressed(): # If A is pressed, stop moving and leave the main loop.
                 bot.left(0)
                 bot.right(0)
                 break
