ff78e7a0c03c5638f6e592c766ad1506413c2e64
DigitalRodent <rendaroadkill@gmail.com>
2026-08-06 12:09:39 +1000

    Diary and Transmitter update

--- Code/Remote Transmitter.py
+++ Code/Remote Transmitter.py
@@ -6,29 +6,28 @@ bot = Maqueen()
 def on_button_pressed_a():
     global signal
     signal = 1
-input.on_button_pressed(Button.A, on_button_pressed_a)
+input.on_button_pressed(button_a, on_button_pressed_a)
 
 def on_button_pressed_ab():
     global signal
     signal = 0
-input.on_button_pressed(Button.AB, on_button_pressed_ab)
+input.on_button_pressed(button_ab, on_button_pressed_ab)
 
 def on_button_pressed_b():
     global signal
     signal = 2
-input.on_button_pressed(Button.B, on_button_pressed_b)
+input.on_button_pressed(button_b, on_button_pressed_b)
 
 signal = 0
 radio.set_group(1)
 signal = 0
+
+def on_forever():
+    radio.send_number(signal)
+basic.forever(on_forever)
 
 while True:
     if button_a.is_pressed():
         display.show(Image.HAPPY)
     if button_b.is_pressed():
         display.show(Image.SAD)
-        break
-
-def on_forever():
-    radio.send_number(signal)
-basic.forever(on_forever)
