a293e129b965004f6f0490ccb635bb023142bb10
jredb <liveofdead5@hotmail.com>
2026-08-06 10:10:33 +1000

    Line following code updated - Algorithm now spins in circles for 2500 counts then begins sweeping algorithm. Going to add count that checks sweeps so if enough are made it turns around and tries the next direction. LOL!

--- Code/objectavoidance.py
+++ Code/objectavoidance.py
@@ -6,3 +6,11 @@ import music
 def avoid():
     distance_in_cm = bot.ultrasound_measure()
     print(distance_in_cm)
+    bot.left(0)
+    bot.right(50)
+    u.sleep(0.85)
+    bot.left(50)
+    u.sleep(0.5)
+    bot.right(0)
+    u.sleep(0.85)
+    bot.left(0)
